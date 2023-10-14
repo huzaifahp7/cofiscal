726377a7b9b744e393fbdb3ff298a0b4f8118db5
Farzad Ashfak <116588610+farzadashfak@users.noreply.github.com>
2023-10-14 05:33:09 -0400

    Update lightgbm_script.py

--- models/lightgbm_script.py
+++ models/lightgbm_script.py
@@ -1,55 +1,66 @@
 import lightgbm as lgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
-# Load your dataset
-data = pd.read_csv('data/Resampled.csv')
+# Load the dataset
+data = pd.read_csv('/content/sample_data/Resampled.csv')
 
-# Calculate the number of rows with Default=1
-num_ones = len(data[data['Default'] == 1])
-num_zeros = len(data[data['Default'] == 0])
+# Define features and target variable
+X = data.drop(columns=['Ind','Default'])
+y = data['Default']
 
-# Randomly sample 50% of 1s and 0s
-sampled_ones = data[data['Default'] == 1].sample(n=num_ones // 2, random_state=42)
-sampled_zeros = data[data['Default'] == 0].sample(n=num_zeros // 2, random_state=42)
+# Split the data into training, testing, and validation sets (70:20:10)
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+X_test, X_val, y_test, y_val = train_test_split(X_temp, y_temp, test_size=0.333, random_state=42)
 
-# Combine the selected rows to create a balanced dataset
-balanced_data = pd.concat([sampled_ones, sampled_zeros])
-
-# Define your features and target variable
-X = balanced_data.drop(columns=['Ind','Default'])
-y = balanced_data['Default']
-
-# Split the data into training and testing sets (70:30)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-# Create LightGBM datasets for train and test
+# Create LightGBM datasets for train, test, and validation
 train_data = lgb.Dataset(X_train, label=y_train)
 test_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+val_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
 
 # Set hyperparameters
 params = {
     "objective": "binary",
     "metric": "binary_logloss",
     "boosting_type": "gbdt",
-    "num_leaves": 31,
+    "num_leaves": 40,
     "learning_rate": 0.05,
     "feature_fraction": 0.9,
 }
 
 # Train the LightGBM model
-num_round = 100
-bst = lgb.train(params, train_data, num_round, valid_sets=[test_data])
+num_round = 550
+bst = lgb.train(params, train_data, num_round, valid_sets=[test_data, val_data])
+
+# Get probabilistic predictions on the test and validation sets
+y_test_prob = bst.predict(X_test, num_iteration=bst.best_iteration)
+y_val_prob = bst.predict(X_val, num_iteration=bst.best_iteration)
+
+# Metrics on test and validation sets
+test_accuracy = accuracy_score(y_test, (y_test_prob >= 0.5).astype(int))
+val_accuracy = accuracy_score(y_val, (y_val_prob >= 0.5).astype(int))
+test_precision = precision_score(y_test, (y_test_prob >= 0.5).astype(int))
+val_precision = precision_score(y_val, (y_val_prob >= 0.5).astype(int))
+test_recall = recall_score(y_test, (y_test_prob >= 0.5).astype(int))
+val_recall = recall_score(y_val, (y_val_prob >= 0.5).astype(int))
+test_f1 = f1_score(y_test, (y_test_prob >= 0.5).astype(int))
+val_f1 = f1_score(y_val, (y_val_prob >= 0.5).astype(int))
+
+print("Test Set Metrics:")
+print(f"Accuracy: {test_accuracy}")
+print(f"Precision: {test_precision}")
+print(f"Recall: {test_recall}")
+print(f"F1 Score: {test_f1}")
 
 # Get probabilistic predictions on the test set
 y_test_prob = bst.predict(X_test, num_iteration=bst.best_iteration)
 
 # Calculate evaluation metrics for the test set
-test_accuracy = accuracy_score(y_test, (y_test_prob >= 0.5).astype(int))
-test_precision = precision_score(y_test, (y_test_prob >= 0.5).astype(int))
-test_recall = recall_score(y_test, (y_test_prob >= 0.5).astype(int))
-test_f1 = f1_score(y_test, (y_test_prob >= 0.5).astype(int))
+test_accuracy = accuracy_score(y_test, (y_test_prob >= 0.45).astype(int))
+test_precision = precision_score(y_test, (y_test_prob >= 0.45).astype(int))
+test_recall = recall_score(y_test, (y_test_prob >= 0.45).astype(int))
+test_f1 = f1_score(y_test, (y_test_prob >= 0.45).astype(int))
 print("Test Set Metrics:")
 print(f"Accuracy: {test_accuracy}")
 print(f"Precision: {test_precision}")
@@ -58,13 +69,13 @@ print(f"F1 Score: {test_f1}")
 
 # Add probabilities to the test set DataFrame
 test_set_with_probs = X_test.copy()
-test_set_with_probs['Prediction'] = (y_test_prob >= 0.5).astype(int)
+test_set_with_probs['Prediction'] = (y_test_prob >= 0.45).astype(int)
 test_set_with_probs['Prob_0'] = 1 - y_test_prob
 test_set_with_probs['Prob_1'] = y_test_prob
 test_set_with_probs['Default'] = y_test  # Add 'Default' back
 
 # Save the test set with probabilities to a CSV file
-test_set_with_probs.to_csv('output/test.csv', index=False)
+test_set_with_probs.to_csv('test_epochs.csv', index=False)
 
 # Create a test set with remaining 0s and 1s
 unseen_X = data.loc[~data.index.isin(balanced_data.index)].drop(columns=['Ind','Default'])
@@ -74,20 +85,20 @@ unseen_y = data.loc[~data.index.isin(balanced_data.index)]['Default']
 unseen_test_prob = bst.predict(unseen_X, num_iteration=bst.best_iteration)
 
 # Calculate evaluation metrics for the unseen test set
-unseen_test_accuracy = accuracy_score(unseen_y, (unseen_test_prob >= 0.5).astype(int))
-unseen_test_precision = precision_score(unseen_y, (unseen_test_prob >= 0.5).astype(int))
-unseen_test_recall = recall_score(unseen_y, (unseen_test_prob >= 0.5).astype(int))
-unseen_test_f1 = f1_score(unseen_y, (unseen_test_prob >= 0.5).astype(int))
+unseen_test_accuracy = accuracy_score(unseen_y, (unseen_test_prob >= 0.45).astype(int))
+unseen_test_precision = precision_score(unseen_y, (unseen_test_prob >= 0.45).astype(int))
+unseen_test_recall = recall_score(unseen_y, (unseen_test_prob >= 0.45).astype(int))
+unseen_test_f1 = f1_score(unseen_y, (unseen_test_prob >= 0.45).astype(int))
 
 # Add probabilities to the unseen test set DataFrame
 unseen_test_set_with_probs = unseen_X.copy()
-unseen_test_set_with_probs['Prediction'] = (unseen_test_prob >= 0.5).astype(int)
+unseen_test_set_with_probs['Prediction'] = (unseen_test_prob >= 0.45).astype(int)
 unseen_test_set_with_probs['Prob_0'] = 1 - unseen_test_prob
 unseen_test_set_with_probs['Prob_1'] = unseen_test_prob
 unseen_test_set_with_probs['Default'] = unseen_y  # Add 'Default' back
 
-# Save the unseen test set with probabilities to a CSV file
-unseen_test_set_with_probs.to_csv('output/unseen_test.csv', index=False)
+# Save the unseen test set with probabilities
+unseen_test_set_with_probs.to_csv('unseen_test.csv', index=False)
 
 # Create DataFrames for metrics
 test_metrics = pd.DataFrame({
@@ -101,8 +112,8 @@ unseen_test_metrics = pd.DataFrame({
 })
 
 # Save metrics to CSV files
-test_metrics.to_csv('output/test_metrics.csv', index=False)
-unseen_test_metrics.to_csv('output/unseen_test_metrics.csv', index=False)
+test_metrics.to_csv('test_metrics_epochs.csv', index=False)
+unseen_test_metrics.to_csv('unseen_test_metrics_epochs.csv', index=False)
 print("\nUnseen Test Set Metrics:")
 print(f"Accuracy: {unseen_test_accuracy}")
 print(f"Precision: {unseen_test_precision}")
